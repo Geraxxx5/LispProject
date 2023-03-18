3e032f25065d27c241dd19736adc7e0411a8f230
Geraxxx5 <gerax.pineda@gmail.com>
2023-03-17 18:40:33 -0600

    The controller package was created with the main function evaluate, which is the one that executes the lisp commands, and a function that changes the key from variables to its value. added javadoc and comments for the functions

--- Project/proyectoalgoritmoslisp/ProyectoAlgoritmosLisp.java
+++ Project/proyectoalgoritmoslisp/ProyectoAlgoritmosLisp.java
@@ -1,5 +1,10 @@
 package proyectoalgoritmoslisp;
 
+import controller.EvaluateExpression;
+import java.util.List;
+import modelo.FunctionModel;
+import modelo.VariableModel;
+import parser.ParserExpression;
 
 public class ProyectoAlgoritmosLisp {
 
@@ -7,7 +12,22 @@ public class ProyectoAlgoritmosLisp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+	//En el dataToTest esta la sintaxis y las lineas para probar en el programa
+        //Se ingresan en el datos.txt (solo lee una linea)
+        FunctionModel functionModel = new FunctionModel();
+        VariableModel variableModel = new VariableModel();
+        EvaluateExpression evaluate = new EvaluateExpression();
+        List<Object> expression = ParserExpression.readFileExpression("datos.txt", true);
+        System.out.println("---------------------------------");
+        System.out.println("obtained expression: "+expression);
+        System.out.println("---------------------------------");
+        System.out.println("---------------------------------");
+        try {
+            System.out.println("result of the expression: "+evaluate.evaluate(expression, variableModel, functionModel));   
+        } catch (Exception e) {
+            System.err.println("Error: "+e);
+        }
+        System.out.println("---------------------------------");
     }
     
 }
